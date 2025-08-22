6506e25c62ad0b1eb63da1227e0b5c3c48211c97
YSW-Yang <tjddnjs6331@naver.com>
2025-08-22 23:32:52 +0900

    [level 2] Title: 주식가격, Time: 18.01 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        
+        for(int i = 0; i < prices.length; i++){
+            int time = 0;
+            for(int j = i + 1; j < prices.length; j++){
+                time++;
+                if(prices[i] > prices[j] || j == prices.length - 1){
+                    answer[i] = time;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
