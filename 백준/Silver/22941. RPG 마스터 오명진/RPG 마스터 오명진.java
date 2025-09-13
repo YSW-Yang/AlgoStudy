5a7eee1a2db839e0d8c3c327fddc02040842facf
YSW-Yang <tjddnjs6331@naver.com>
2025-09-13 13:55:24 +0900

    Update RPG 마스터 오명진.java
    다시 풀어봄

--- 백준/Silver/22941. RPG 마스터 오명진/RPG 마스터 오명진.java
+++ 백준/Silver/22941. RPG 마스터 오명진/RPG 마스터 오명진.java
@@ -1,53 +1,84 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+// 다시 풀었다
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] strArr = br.readLine().split(" ");
 		long playerHp = Integer.parseInt(strArr[0]);
-		long playerAtk = Integer.parseInt(strArr[1]);
+		int playerAtk = Integer.parseInt(strArr[1]);
 		long demonHp = Integer.parseInt(strArr[2]);
-		long maxDemonHp = demonHp;
 		int demonAtk = Integer.parseInt(strArr[3]);
 		strArr = br.readLine().split(" ");
-		long P = Integer.parseInt(strArr[0]);
-		long S = Integer.parseInt(strArr[1]);
+		int P = Integer.parseInt(strArr[0]);
+		int S = Integer.parseInt(strArr[1]);
 
-		long turn = 0;
-		if ((demonHp - P) % playerAtk == 0 && demonHp - P != 0) {
-			turn = (demonHp - P) / playerAtk - 1;
-		} else {
-			turn = (demonHp - P) / playerAtk;
-		}
+		long playerDethCount = playerHp % demonAtk == 0 ? playerHp / demonAtk : playerHp / demonAtk + 1;
+		long demonDethCount = (demonHp - P) % playerAtk == 0 ? (demonHp - P) / playerAtk
+				: (demonHp - P) / playerAtk + 1;
 
-		demonHp -= (turn + 1) * playerAtk;
-		playerHp -= turn * demonAtk;
-		if (playerHp <= 0) {
-			System.out.println("gg");
-			return;
-		} else if (demonHp <= 0) {
+		demonHp -= (demonDethCount) * playerAtk;
+		demonHp = demonHp <= 0 ? 0 : demonHp + S;
+		demonDethCount += demonHp % playerAtk == 0 ? demonHp / playerAtk : demonHp / playerAtk + 1;
+
+		if (playerDethCount >= demonDethCount) {
 			System.out.println("Victory!");
-			return;
-		} else if (demonHp >= 1 && demonHp <= P) {
-			playerHp -= demonAtk;
-			if (maxDemonHp < demonHp + S) {
-				demonHp += maxDemonHp;
-			} else {
-				demonHp += S;
-			}
-		}
-
-		if (demonHp % playerAtk == 0) {
-			turn = demonHp / playerAtk - 1;
 		} else {
-			turn = demonHp / playerAtk;
-		}
-
-		if (playerHp - turn * demonAtk <= 0) {
 			System.out.println("gg");
-		} else {
-			System.out.println("Victory!");
 		}
 	}
 }
+
+
+// public class Main {
+// 	public static void main(String[] args) throws Exception {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		String[] strArr = br.readLine().split(" ");
+// 		long playerHp = Integer.parseInt(strArr[0]);
+// 		long playerAtk = Integer.parseInt(strArr[1]);
+// 		long demonHp = Integer.parseInt(strArr[2]);
+// 		long maxDemonHp = demonHp;
+// 		int demonAtk = Integer.parseInt(strArr[3]);
+// 		strArr = br.readLine().split(" ");
+// 		long P = Integer.parseInt(strArr[0]);
+// 		long S = Integer.parseInt(strArr[1]);
+
+// 		long turn = 0;
+// 		if ((demonHp - P) % playerAtk == 0 && demonHp - P != 0) {
+// 			turn = (demonHp - P) / playerAtk - 1;
+// 		} else {
+// 			turn = (demonHp - P) / playerAtk;
+// 		}
+
+// 		demonHp -= (turn + 1) * playerAtk;
+// 		playerHp -= turn * demonAtk;
+// 		if (playerHp <= 0) {
+// 			System.out.println("gg");
+// 			return;
+// 		} else if (demonHp <= 0) {
+// 			System.out.println("Victory!");
+// 			return;
+// 		} else if (demonHp >= 1 && demonHp <= P) {
+// 			playerHp -= demonAtk;
+// 			if (maxDemonHp < demonHp + S) {
+// 				demonHp += maxDemonHp;
+// 			} else {
+// 				demonHp += S;
+// 			}
+// 		}
+
+// 		if (demonHp % playerAtk == 0) {
+// 			turn = demonHp / playerAtk - 1;
+// 		} else {
+// 			turn = demonHp / playerAtk;
+// 		}
+
+// 		if (playerHp - turn * demonAtk <= 0) {
+// 			System.out.println("gg");
+// 		} else {
+// 			System.out.println("Victory!");
+// 		}
+// 	}
+
+// }
