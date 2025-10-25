c94948cbbaf96154fb2d4c665ac1a199ea036c42
YSW-Yang <tjddnjs6331@naver.com>
2025-10-25 23:12:19 +0900

    [Silver I] Title: 문자열 교환, Time: 96 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/1522. 문자열 교환/문자열 교환.java
+++ 백준/Silver/1522. 문자열 교환/문자열 교환.java
@@ -2,37 +2,33 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String s = br.readLine();
-		int aTotalSize = 0;
-		int bSize = 0;
-		int result = Integer.MAX_VALUE;
+		int aCount = 0;
+		int bCurrentCount = 0;
+		int result = 0;
 
 		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) == 'a') {
-				aTotalSize++;
-			}
+			if (s.charAt(i) == 'a')
+				aCount++;
 		}
 
-		for (int i = 0; i < aTotalSize; i++) {
-			if (s.charAt(i) == 'b') {
-				bSize++;
-			}
+		for (int i = 0; i < aCount; i++) {
+			if (s.charAt(i) == 'b')
+				bCurrentCount++;
 		}
-		result = bSize;
+		result = bCurrentCount;
 
 		for (int i = 1; i < s.length(); i++) {
 			if (s.charAt(i - 1) == 'b')
-				bSize--;
+				bCurrentCount--;
 
-			if (s.charAt((i + aTotalSize - 1) % s.length()) == 'b')
-				bSize++;
+			if (s.charAt((i + aCount - 1) % s.length()) == 'b')
+				bCurrentCount++;
 
-			if (result > bSize)
-				result = bSize;
-
+			if (result > bCurrentCount)
+				result = bCurrentCount;
 		}
 
 		System.out.println(result);
