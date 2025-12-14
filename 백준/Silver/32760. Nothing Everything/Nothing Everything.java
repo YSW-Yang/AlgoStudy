7c33e6b891973d325e14bf1558881ed5a86d4b76
YSW-Yang <tjddnjs6331@naver.com>
2025-12-14 21:55:30 +0900

    [Silver I] Title: Nothing Everything, Time: 440 ms, Memory: 48256 KB -BaekjoonHub

--- 백준/Silver/32760. Nothing Everything/Nothing Everything.java
+++ 백준/Silver/32760. Nothing Everything/Nothing Everything.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
@@ -11,31 +9,22 @@ public class Main {
 		int N = Integer.parseInt(inputs[0]);
 		int M = Integer.parseInt(inputs[1]);
 
-		ArrayList<Integer>[] graph = new ArrayList[N + 1];
-
-		for (int i = 1; i <= N; i++) {
-			graph[i] = new ArrayList<Integer>();
-		}
-
+		int[] cnt = new int[100001];
 		for (int i = 0; i < M; i++) {
 			inputs = br.readLine().split(" ");
 			int from = Integer.parseInt(inputs[0]);
 			int to = Integer.parseInt(inputs[1]);
-			graph[from].add(to);
-			graph[to].add(from);
+			if (from > to) {
+				cnt[from]++;
+			} else {
+				cnt[to]++;
+			}
 		}
 
 		for (int i = 2; i <= N; i++) {
-			int count = 0;
-			for (int j = 0; j < graph[i].size(); j++) {
-				if (graph[i].get(j) < i) {
-					count++;
-				}
-			}
-
-			if (count == 0) {
+			if (cnt[i] == 0) {
 				sb.append("N");
-			} else if (count == i - 1) {
+			} else if (cnt[i] == i - 1) {
 				sb.append("E");
 			} else {
 				System.out.println(-1);
