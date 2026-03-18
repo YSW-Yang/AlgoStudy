91b1db6efccb3fa42b545cb49768b2a02d738115
YSW-Yang <tjddnjs6331@naver.com>
2026-03-18 18:46:27 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.96 ms, Memory: 97.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -5,23 +5,17 @@ class Solution {
         String answer = "";
         HashMap<String, Integer> map = new HashMap<String, Integer>();
         
-        for(int i = 0; i < participant.length; i++){
-            if(map.containsKey(participant[i])){
-                map.put(participant[i], map.get(participant[i]) + 1);
-            }else{
-                map.put(participant[i], 1);
-            }
+        for(String p : participant){
+            map.put(p, map.getOrDefault(p, 0) + 1);
         }
         
-        for(int i = 0; i < completion.length; i++){
-            if(map.containsKey(completion[i])){
-                map.put(completion[i], map.get(completion[i]) - 1);
-            }
+        for(String c : completion){
+            map.put(c, map.get(c) - 1);
         }
         
-        for(int i = 0; i < participant.length; i++){
-            if(map.get(participant[i]) == 1){
-                answer = participant[i];
+        for(String key : map.keySet()){
+            if(map.get(key) == 1){
+                answer = key;
             }
         }
         
