a94bf98da73ad22ee4e00ecd62d38c84b1520329
YSW-Yang <tjddnjs6331@naver.com>
2026-03-21 01:36:22 +0900

    [level 3] Title: 순위, Time: 18.51 ms, Memory: 97.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int solution(int n, int[][] results) {
+        int answer = 0;
+        boolean[][] wins = new boolean[n + 1][n + 1];
+        
+        for(int[] result : results) {
+            wins[result[0]][result[1]] = true;
+        }
+        
+        for(int k = 1; k <= n; k++) {
+            for(int i = 1; i <= n; i++) {
+                for(int j = 1; j <= n; j++) {
+                    if(wins[i][k] && wins[k][j]) {
+                        wins[i][j] = true;
+                    }
+                }
+            }
+        }
+        
+        for(int i = 1; i <= n; i++) {
+            int count = 0;
+            for(int j = 1; j <= n; j++) {
+                if(wins[i][j] || wins[j][i]) {
+                    count++;
+                }
+            }
+            if(count == n - 1) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
