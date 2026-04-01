027c317130c8230edaa223ad16f719dcaa7631cb
YSW-Yang <tjddnjs6331@naver.com>
2026-04-01 13:35:30 +0900

    [Gold V] Title: 회의실 배정, Time: 480 ms, Memory: 43240 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[][] meetings = new int[N][2];
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			meetings[i][0] = Integer.parseInt(st.nextToken());
+			meetings[i][1] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(meetings, (t1, t2) -> {
+			if (t1[1] == t2[1]) {
+				return t1[0] - t2[0];
+			}
+			return t1[1] - t2[1];
+		});
+
+		int count = 0;
+		int prevEndTime = 0;
+		for (int i = 0; i < N; i++) {
+			if (prevEndTime <= meetings[i][0]) {
+				prevEndTime = meetings[i][1];
+				count++;
+			}
+		}
+
+		System.out.println(count);
+	}
+}
