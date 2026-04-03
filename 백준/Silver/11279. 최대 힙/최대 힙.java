2dd3357936559e20851b81ce0ab6b24ec9cee80c
YSW-Yang <tjddnjs6331@naver.com>
2026-04-03 15:33:01 +0900

    [Silver II] Title: 최대 힙, Time: 288 ms, Memory: 26496 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(br.readLine());
+
+			if (num == 0) {
+				sb.append(pq.isEmpty() ? 0 : pq.poll()).append("\n");
+			} else {
+				pq.add(num);
+			}
+		}
+
+		System.out.println(sb);
+	}
+}
